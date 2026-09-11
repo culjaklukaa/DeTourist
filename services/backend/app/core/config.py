102ead5fc002606b20269a315ebcd0d49b678fa2
LENOVO <culjakluka9@gmail.com>
2026-09-11 21:38:22 +0200

    Network connection bug fix

--- services/backend/app/core/config.py
+++ services/backend/app/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "DeTourist API"
-    API_V1_STR: str = "/api/v1"
+    API_V1_STR: str = "/v1"
     
     # Environment
     ENVIRONMENT: Literal["local", "staging", "prod"] = "local"
